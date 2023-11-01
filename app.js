8286697bdcba9994e13c4d9aa2700d5403a4352f
S567541 <S567541@nwmissouri.edu>
2023-11-01 16:29:56 -0500

    Whistle v2

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var WhistleRouter = require('./routes/Whistle');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+
 
 var app = express();
 
@@ -22,7 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Whistle',WhistleRouter)
+app.use('/Whistle',WhistleRouter);
+app.use('/board',boardRouter);
+app.use('/choose',chooseRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
